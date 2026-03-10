ae18d60a4e5bbb51e704723aa402ea9e4a60b767
nandiniadapa <nnandiniadapa@gmail.com>
2026-03-10 13:59:25 +0530

    temperature converter

---
+++ TemperatureConverter.js
@@ -0,0 +1,7 @@
+function onClickConverterFToC() {
+    debugger;
+    cTemperature = parseFloat(document.getElementById("txttemperature").value);
+    fTemperature = cTemperature * 1.8 + 32
+    document.getElementById("temperatureResult").innerHTML = "result:" + fTemperature + "f"
+
+}
