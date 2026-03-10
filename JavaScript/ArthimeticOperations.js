cc86953e6e7c4bf10008e23be36cca27d421b959
nandiniadapa <nnandiniadapa@gmail.com>
2026-03-10 13:40:43 +0530

    arthimetic operations

---
+++ JavaScript/ArthimeticOperations.js
@@ -0,0 +1,28 @@
+function onClickAdd() {
+                               debugger;
+                               var value1 = parseInt(document.getElementById("txtValue1").value);
+                               var value2 = parseInt(document.getElementById("txtValue2").value);
+                                result = value1 + value2;
+                                document.getElementById("pResult").innerHTML = result
+                        }
+                        function onClickSub() {
+                                debugger;
+                                var value1 = parseInt(document.getElementById("txtValue1").value);
+                                var value2 = parseInt(document.getElementById("txtValue2").value);
+                                result = value1 - value2;
+                                document.getElementById("pResult").innerHTML = result
+                        }
+                        function onClicKMultify() {
+                                debugger;
+                                var value1 = parseInt(document.getElementById("txtValue1").value);
+                                var value2 = parseInt(document.getElementById("txtValue2").value);
+                                result = value1 * value2;
+                                document.getElementById("pResult").innerHTML = result
+                        }
+                        function onClickDiv(){
+                                debugger;
+                                 var value1 = parseInt(document.getElementById("txtValue1").value);
+                                 var value2 = parseInt(document.getElementById("txtValue2").value);
+                                 result= value1/value2;
+                                  document.getElementById("pResult").innerHTML= result
+                        }
