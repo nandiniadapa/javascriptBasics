55315b3a8eae516c7cc9cb8b265d3e01f5d48853
nandiniadapa <nnandiniadapa@gmail.com>
2026-03-15 16:14:30 +0530

    Fixed value

---
+++ ToFixed.js
@@ -0,0 +1,6 @@
+function onClickFixednumber(){
+    debugger;
+     decimalvalue=Number(document.getElementById("txtDecimalnumber").value);
+     fixedvalue=Number(document.getElementById("txtFixnumber").value);
+     document.getElementById("pResult").innerHTML=DecimalNumber.toFixed(FixNumber)
+}
