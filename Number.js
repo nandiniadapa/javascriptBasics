8c7238e9231c02d1db632669c40fcb022d3d56e1
nandiniadapa <nnandiniadapa@gmail.com>
2026-03-15 15:17:16 +0530

    feat(Number.html)

---
+++ Number.js
@@ -0,0 +1,6 @@
+function onClickNumberfunction(){
+
+    debugger;
+    var result = Number(document.getElementById("txtValue").value);
+    document.getElementById("pResult").innerHTML = result;
+}
