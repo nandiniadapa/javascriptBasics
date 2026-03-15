eca9ffd0656a124392a8f55580213f6ce727db3e
nandiniadapa <nnandiniadapa@gmail.com>
2026-03-15 12:43:26 +0530

    feat(parsInt.html) in vs code

---
+++ ParseInt.js
@@ -0,0 +1,5 @@
+function onClickParseIntChecker() {
+    debugger;
+    var result = parseInt(document.getElementById("txtValue").value);
+    document.getElementById("pResult").innerHTML = result;
+}
