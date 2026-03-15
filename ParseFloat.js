bb0139f1f1fb85eabbad255d966ca08e4020e324
nandiniadapa <nnandiniadapa@gmail.com>
2026-03-15 13:32:05 +0530

    feat(ParseFloat.html):updated in vs code

--- ParseFloat.js
+++ ParseFloat.js
@@ -3,4 +3,11 @@ function onClickParseFloat(){
     var result = parseFloat(document.getElementById("txtValue").value);
     document.getElementById("pResult").innerHTML=result ;
 }
-   
+   document.getElementById("pDemo").innerHTML = 
+  Number.parseInt("-10") + "<br>" +
+  Number.parseInt("-10.33") + "<br>" +
+  Number.parseInt("10") + "<br>" +
+  Number.parseInt("10.33") + "<br>" +
+  Number.parseInt("10 6") + "<br>" +  
+  Number.parseInt("10 years") + "<br>" +  
+  Number.parseInt("years 10");  
