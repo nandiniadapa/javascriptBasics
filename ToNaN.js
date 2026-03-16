92a3ea0b8cd490afd476c37bf08cbeb739004174
nandiniadapa <nnandiniadapa@gmail.com>
2026-03-16 19:50:07 +0530

    updated

---
+++ ToNaN.js
@@ -0,0 +1,6 @@
+function onClickNaNnumber(){
+    debugger;
+     decimalValue=Number(document.getElementById("txtDecimalnumber").value);
+     fixedValue=Number(document.getElementById("txtFixnumber").value);
+     document.getElementById("pResult").innerHTML=decimalValue.Number(fixedValue)
+}
