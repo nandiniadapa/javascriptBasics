594e81ccdb5387d8910f0b2389160dd806b2c67c
nandiniadapa <nnandiniadapa@gmail.com>
2026-03-17 19:59:13 +0530

    NaN function

--- ToNaN.js
+++ ToNaN.js
@@ -1,6 +1,12 @@
-function onClickNaNnumber(){
+function onClickNaNnumber() {
     debugger;
-     decimalValue=Number(document.getElementById("txtDecimalnumber").value);
-     fixedValue=Number(document.getElementById("txtFixnumber").value);
-     document.getElementById("pResult").innerHTML=decimalValue.Number(fixedValue)
+    let decimalValue = document.getElementById("txtDecimalValue").value;
+    let fixNumber = document.getElementById("txtFixnumber").value;
+    decimalValue = parseFloat(decimalValue);
+    if (isNaN(decimalValue)) {
+        document.getElementById("pResult").innerText = "The value is NOT a number (NaN)";
+    } else {
+        let result = decimalValue.toFixed(fixNumber);
+        document.getElementById("pResult").innerText = "Result: " + result;
+    }
 }
