6050f4d11687c92e841b0e2739af911c3caf4d58
nandiniadapa <nnandiniadapa@gmail.com>
2026-03-21 12:02:03 +0530

    ToNaN Function

--- ToNaN.js
+++ ToNaN.js
@@ -1,12 +1,25 @@
-function onClickNaNnumber() {
+// function onClickNaNnumber() {
+//     debugger;
+//     let decimalValue = document.getElementById("txtDecimalValue").value;
+//     let fixNumber = document.getElementById("txtFixnumber").value;
+//     decimalValue = parseFloat(decimalValue);
+//     if (isNaN(decimalValue)) {
+//         document.getElementById("pResult").innerText = "The value is NOT a number (NaN)";
+//     } else {
+//         let result = decimalValue.toFixed(fixNumber); 
+//         document.getElementById("pResult").innerText = "Result: " + result;
+//     }
+// }
+
+
+ function onClickIsNaNnumber(){
     debugger;
-    let decimalValue = document.getElementById("txtDecimalValue").value;
-    let fixNumber = document.getElementById("txtFixnumber").value;
-    decimalValue = parseFloat(decimalValue);
-    if (isNaN(decimalValue)) {
-        document.getElementById("pResult").innerText = "The value is NOT a number (NaN)";
-    } else {
-        let result = decimalValue.toFixed(fixNumber); 
-        document.getElementById("pResult").innerText = "Result: " + result;
-    }
-}     
+    var value1=document.getElementById("txtValue1").value;
+    var value2=document.getElementById("txtvalue2").value;
+    var firstValueisNaN=isNaN(value1);
+    var secondValueisNaN=isNaN(value2);
+     console.log(firstValueisNaN);
+     console.log(secondValueisNaN);
+     
+
+ }
