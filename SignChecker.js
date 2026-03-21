48e074077de9fb0691cc30d10ce84ae0f2af4ec9
nandiniadapa <nnandiniadapa@gmail.com>
2026-03-21 16:11:44 +0530

    signchecker

---
+++ SignChecker.js
@@ -0,0 +1,7 @@
+function onClickSignchecker(){
+    debugger;
+     var inputValue= document.getElementById("txtinputValue").value;
+     document.getElementById("pResult").innerHTML=Math.sign (inputValue);
+
+
+}
