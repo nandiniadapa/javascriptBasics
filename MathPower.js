ec936f4d2e2a2b3df3283a0d1ae4eac088dcd7ce
nandiniadapa <nnandiniadapa@gmail.com>
2026-03-21 16:43:47 +0530

    Math sqrt functions

--- MathPower.js
+++
@@ -1,7 +0,0 @@
-function onClickPowerChecker(){
-    debugger;
-    var Value=document.getElementById("txtValue").value;
-    var power=document.getElementById("txtPower").value;
-    document.getElementById("pResult").innerHTML= Math.pow ( power,Value);
-
-}
