86510fbfe8c2924c0bea1b4c3702bbae3ea353bb
nandiniadapa <nnandiniadapa@gmail.com>
2026-03-22 13:24:48 +0530

    feat(MathRandom.html):resolving random functions

--- MathRandom.js
+++ MathRandom.js
@@ -1,6 +1,6 @@
 function onClickGenerateRandom(){
     debugger;
     var sizeNumber= Number(document.getElementById("txtInput" ).value);
-    var randomNumber= Math.round(math.random()* Math.pow(10,sizeNumber));
-    document.getElementById("pResult").innerHTML="Random Number" + randomNumber
+    var randomNumber= Math.round(Math.random()* Math.pow(10,sizeNumber));
+    document.getElementById("pResult").innerHTML="Random Number: " + randomNumber;
 }
