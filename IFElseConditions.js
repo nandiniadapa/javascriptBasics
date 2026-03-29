0ef8d92d8930b72f1f6a56d02f43c617838f8faa
nandiniadapa <nnandiniadapa@gmail.com>
2026-03-29 13:22:33 +0530

    IFElseconditions

--- IFElseConditions.js
+++ IFElseConditions.js
@@ -1,11 +1,26 @@
-function onClickIFElseConditions() {
+// function onClickIFElseConditions() {
+//     debugger;
+//     var Number = parseFloat(document.getElementById("txtNumber").value);
+//     if (Number > 0) {
+//         document.getElementById("pResult").innerHTML = "The number is positive.";
+//     } else if (Number < 0) {
+//         document.getElementById("pResult").innerHTML = "The number is negative.";
+//     } else {
+//         document.getElementById("pResult").innerHTML = "The number is zero.";
+//     }
+// }
+function onClickIFElseConditions(){
     debugger;
-    var Number = parseFloat(document.getElementById("txtNumber").value);
-    if (Number > 0) {
-        document.getElementById("pResult").innerHTML = "The number is positive.";
-    } else if (Number < 0) {
-        document.getElementById("pResult").innerHTML = "The number is negative.";
-    } else {
-        document.getElementById("pResult").innerHTML = "The number is equal to 0.";
+    var Number=parseInt(document.getElementById("txtNumber").value);
+    if((Number%3==0) && (Number%7==0)){
+        document.getElementById("pResult").innerHTML="The number is divisible by  3 and 7.";
+        console.log("The number is divisible 3 and 7. ");
+     }
+     else if((Number%3==0) || (Number%7==0)){
+        document.getElementById("pResult").innerHTML="The number is divisible by 3 or 7.";
     }
+    else{
+        document.getElementById("pResult").innerHTML="The number is not divisible by 3 and 7.";
+        console.log("The number is not divisible by 3 and 7. ");
+     }
 }
