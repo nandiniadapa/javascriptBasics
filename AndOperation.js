f2fb92bc7a621afa101045af3c9133443fe7d113
nandiniadapa <nnandiniadapa@gmail.com>
2026-03-29 11:41:07 +0530

    checkoperation

--- AndOperation.js
+++ AndOperation.js
@@ -1,7 +1,11 @@
 function onClickAndOperation(){
-     var Number= parseFloat(document.getElementById("txtNumber").value)
+    debugger;
+     var Number= parseFloat(document.getElementById("txtNumber").value);
      if((Number%3==0) && (Number%5==0)){
         document.getElementById("pResult").innerHTML="The number is divisible by  3 and 5.";
         console.log("The number is divisible 3 and 5. ");
+     }else{
+        document.getElementById("pResult").innerHTML="The number is not divisible by 3 and 5.";
+        console.log("The number is not divisible by 3 and 5. ");
      }
  }
