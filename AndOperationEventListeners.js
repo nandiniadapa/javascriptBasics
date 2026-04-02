da8bc81d2df711c26bbcbd911ef2cdb3ac89cc17
nandiniadapa <nnandiniadapa@gmail.com>
2026-04-02 12:04:11 +0530

    feat(AndOpeartionEventListeners.html): linking with js

---
+++ AndOperationEventListeners.js
@@ -0,0 +1,22 @@
+// function onClickAndOperation(){
+//     debugger;
+//      var Number= parseFloat(document.getElementById("txtNumber").value);
+//      if((Number%3==0) && (Number%5==0)){
+//         document.getElementById("pResult").innerHTML="The number is divisible by  3 and 5.";
+//         console.log("The number is divisible 3 and 5. ");
+//      }else{
+//         document.getElementById("pResult").innerHTML="The number is not divisible by 3 and 5.";
+//         console.log("The number is not divisible by 3 and 5. ");
+//      }
+//  }
+ document.getElementById("btnNumber").addEventListener("click", function () {
+    debugger;
+  var Number= parseFloat(document.getElementById("txtNumber").value);
+     if((Number%3==0) && (Number%5==0)){
+        document.getElementById("pResult").innerHTML="The number is divisible by  3 and 5.";
+        console.log("The number is divisible 3 and 5. ");
+     }else{
+        document.getElementById("pResult").innerHTML="The number is not divisible by 3 and 5.";
+        console.log("The number is not divisible by 3 and 5. ");
+     }
+});
