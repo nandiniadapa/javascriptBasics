95509b6ae7eb11263690485c39996b25e449ffcf
nandiniadapa <nnandiniadapa@gmail.com>
2026-04-02 15:59:04 +0530

    finitecheckeventlisteners.js

---
+++ FiniteCheckEventListeners.js
@@ -0,0 +1,10 @@
+    document.getElementById("btnfinitechecker").addEventListener("click", function(){
+    debugger;
+  var inputValue=document.getElementById("txtinput").value;
+  var numericValue=parseInt(inputValue);
+  divisionresult=Infinity/(numericValue);
+   var result=isFinite(divisionresult);
+
+   document.getElementById("pResult").innerHTML="Is the Value Finite"+ result;
+
+});
